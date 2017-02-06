794339cd2387888e9b27bd11ed6d1052d0aa7dc8
Robin Jia <robinjia@stanford.edu>
2017-02-06 07:26:28 -0800

    add

---
+++ data/robinjia.rb
@@ -0,0 +1,100 @@
+entry!('levesque2013best',
+  author('Hector J. Levesque'),
+  title('On our best behaviour'),
+  ijcai(2013),
+nil)
+
+entry!('szegedy2014intriguing',
+  author('Christian Szegedy and Wojciech Zaremba and Ilya Sutskever and Joan Bruna and Dumitru Erhan and Ian Goodfellow and Rob Fergus'),
+  title('Intriguing properties of neural networks'),
+  iclr(2014),
+  url('https://arxiv.org/abs/1312.6199'),
+nil)
+
+entry!('seo2016bidaf',
+  author('Minjoon Seo and Aniruddha Kembhavi and Ali Farhadi and Hannaneh Hajishirzi'),
+  title('Bidirectional Attention Flow for Machine Comprehension'),
+  arxiv(2016),
+  url('https://arxiv.org/abs/1611.01603'),
+nil)
+
+entry!('papernot2017blackbox',
+  author('Nicolas Papernot and Patrick McDaniel and Ian Goodfellow and Somesh Jha and Z.Berkay Celik and Ananthram Swami'),
+  title('Practical Black-Box Attacks against Deep Learning Systems using Adversarial Examples'),
+  inproceedings('Proceedings of the ACM Asia Conference on Computer and Communications Security', 2017),
+  url('https://arxiv.org/abs/1602.02697'),
+nil)
+
+entry!('narodytska2016blackbox',
+  author('Nina Narodytska and Shiva Prasad Kasiviswanathan'),
+  title('Simple Black-Box Adversarial Perturbations for Deep Networks'),
+  arxiv(2016, '1612.06299'),
+nil)
+
+entry!('paice1990lancaster',
+  author('Chris D. Paice'),
+  title('Another Stemmer'),
+  inproceedings('ACM SIGIR Forum', 1990),
+nil)
+
+entry!('marcus1999ptb',
+  author('Mitchell Marcus and Beatrice Santorini and Mary Ann Marcinkiewicz and Ann Taylor'),
+  title('Treebank-3'),
+  organization('Philadelphia: Linguistic Data Consortium'),
+  manual(1999),
+nil)
+
+entry!('francis1979brown',
+  author('W. Nelson Francis and Henry Kucera'),
+  title('Brown Corpus Manual'),
+  manual(1979),
+nil)
+
+entry!('hixon2015dialog',
+  author('Ben Hixon and Peter Clark and Hannaneh Hajishirzi'),
+  title('Learning knowledge graphs for question answering through conversational dialog'),
+  naacl(2015),
+nil)
+
+entry!('smith2012adversarial',
+  author('Noah A. Smith'),
+  title('Adversarial Evaluation for Models of Natural Language'),
+  arxiv(2012, '1207.0245'),
+nil)
+
+entry!('bowman2016continuous',
+  author('Samuel R. Bowman and Luke Vilnis and Oriol Vinyals and Andrew M. Dai and Rafal Jozefowicz and Samy Bengio'),
+  title('Generating Sentences from a Continuous Space'),
+  conll(2016),
+  url('https://arxiv.org/abs/1511.06349'),
+nil)
+
+entry!('li2017adversarial',
+  author('Jiwei Li and Will Monroe and Tianlin Shi and Alan Ritter and Dan Jurafsky'),
+  title('Adversarial Learning for Neural Dialogue Generation'),
+  arxiv(2017, '1701.06547'),
+nil)
+
+entry!('dalvi2004adversarial',
+  author('Nilesh Dalvi and Pedro Domingos and Mausam and Sumit Sanghai and Deepak Verma'),
+  title('Adversarial Classification'),
+  kdd(2004),
+nil)
+
+entry!('lowd2005adversarial',
+  author('Daniel Lowd and Christopher Meek'),
+  title('Adversarial learning'),
+  kdd(2005),
+nil)
+
+entry!('paperno2016lambada',
+  author('Denis Paperno and German Kruszewski and Angeliki Lazaridou and Quan Ngoc Pham and Raffaella Bernardi and Sandro Pezzelle and Marco Baroni and Gemma Boleda and Raquel Fernández'),
+  title('The {LAMBADA} dataset: Word prediction requiring a broad discourse context'),
+  acl(2016),
+nil)
+
+entry!('rimell2009unbounded',
+  author('Laura Rimell and Stephen Clark and Mark Steedman'),
+  title('Unbounded Dependency Recovery for Parser Evaluation'),
+  emnlp(2009),
+nil)
