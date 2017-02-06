9d583cc1e03e18cba8c8306c25af639a7453a0f5
Kelvin Guu <guu.kelvin@gmail.com>
2017-02-05 18:38:29 -0800

    add

---
+++ data/kguu.rb
@@ -0,0 +1,83 @@
+
+entry!('ranzato2015sequence',
+  arxiv(2015, '1511.06732'),
+  title('Sequence level training with recurrent neural networks'),
+  author('Marc\'Aurelio Ranzato and Sumit Chopra and Michael Auli and Wojciech Zaremba'),
+nil)
+
+entry!('li2016deep',
+  arxiv(2016, '1606.01541'),
+  title('Deep reinforcement learning for dialogue generation'),
+  author('Jiwei Li and Will Monroe and Alan Ritter and Michel Galley and Jianfeng Gao and Dan Jurafsky'),
+nil)
+
+entry!('clark2016deep',
+  arxiv(2016, '1609.08667'),
+  title('Deep reinforcement learning for mention-ranking coreference models'),
+  author('Kevin Clark and Christopher D Manning'),
+nil)
+
+entry!('narasimhan2015language',
+  arxiv(2015, '1506.08941'),
+  title('Language understanding for text-based games using deep reinforcement learning'),
+  author('Karthik Narasimhan and Tejas Kulkarni and Regina Barzilay'),
+nil)
+
+entry!('narasimhan2016improving',
+  arxiv(2016, '1603.07954'),
+  title('Improving information extraction by acquiring external evidence with reinforcement learning'),
+  author('Karthik Narasimhan and Adam Yala and Regina Barzilay'),
+nil)
+
+entry!('norouzi2016reward',
+  inproceedings('Advances In Neural Information Processing Systems', 2016),
+  title('Reward augmented maximum likelihood for neural structured prediction'),
+  author('Mohammad Norouzi and Samy Bengio and Navdeep Jaitly and Mike Schuster and Yonghui Wu and Dale Schuurmans and others'),
+  pages(1723, 1731),
+nil)
+
+entry!('osband2016deep',
+  inproceedings('Advances In Neural Information Processing Systems', 2016),
+  title('Deep exploration via bootstrapped {DQN}'),
+  author('Ian Osband and Charles Blundell and Alexander Pritzel and Benjamin Van Roy'),
+  pages(4026, 4034),
+nil)
+
+entry!('nachum2016improving',
+  arxiv(2016, '1611.09321'),
+  title('Improving Policy Gradient by Exploring Under-appreciated Rewards'),
+  author('Ofir Nachum and Mohammad Norouzi and Dale Schuurmans'),
+nil)
+
+entry!('williams1992simple',
+  article('Machine learning', 1992, 8),
+  title('Simple statistical gradient-following algorithms for connectionist reinforcement learning'),
+  author('Ronald J Williams'),
+  number('3-4'),
+  pages(229, 256),
+  publisher('Springer'),
+nil)
+
+entry!('williams1991function',
+  article('Connection Science', 1991, 3),
+  title('Function optimization using connectionist reinforcement learning algorithms'),
+  author('Ronald J Williams and Jing Peng'),
+  number(3),
+  pages(241, 268),
+  publisher('Taylor \& Francis'),
+nil)
+
+entry!('liu2008monte',
+  title('Monte Carlo strategies in scientific computing'),
+  author('Jun S Liu'),
+  year(2008),
+  book('Springer Science \& Business Media', 2008),
+  publisher('Springer Science \& Business Media'),
+nil)
+
+entry!('venkatraman2015improving',
+  aaai(2015),
+  title('Improving Multi-Step Prediction of Learned Time Series Models'),
+  author('Arun Venkatraman and Martial Hebert and J Andrew Bagnell'),
+  pages(3024, 3030),
+nil)
