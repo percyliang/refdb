43c0201496f2b0a5eb6f29b33768bf88153c83f3
Kelvin Guu <guu.kelvin@gmail.com>
2017-02-06 12:55:53 -0800

    add

--- data/kguu.rb
+++ data/kguu.rb
@@ -81,3 +81,17 @@ entry!('venkatraman2015improving',
   author('Arun Venkatraman and Martial Hebert and J Andrew Bagnell'),
   pages(3024, 3030),
 nil)
+
+entry!('bengio2015scheduled',
+  nips(2015),
+  title('Scheduled sampling for sequence prediction with recurrent neural networks'),
+  author('Samy Bengio and Oriol Vinyals and Navdeep Jaitly and Noam Shazeer'),
+  pages(1171, 1179),
+nil)
+
+entry!('bellemare2016unifying',
+  nips(2016),
+  title('Unifying count-based exploration and intrinsic motivation'),
+  author('Marc Bellemare and Sriram Srinivasan and Georg Ostrovski and Tom Schaul and David Saxton and Remi Munos'),
+  pages(1471, 1479),
+nil)
