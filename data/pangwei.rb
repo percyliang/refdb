519f95b50a6ba34220245e88e984af77bea1ce72
Pang Wei Koh <pangwei@cs.stanford.edu>
2017-02-12 21:53:58 -0800

    add

---
+++ data/pangwei.rb
@@ -0,0 +1,52 @@
+# entry!('',
+#   author(''),
+#   title(''),
+#   arxiv(2016, ''),
+#   url(''),
+# nil)
+
+
+entry!('wojnowicz2016sketching',
+  author('Mike Wojnowicz and Ben Cruz and Xuan Zhao and Brian Wallace and Matt Wolff and Jay Luan and Caleb Crable'),
+  title('``{I}nfluence Sketching\'\': Finding Influential Samples In Large-Scale Regressions'),  
+  arxiv(2016, '1611.05923'),
+  url('https://arxiv.org/abs/1611.05923'),
+nil)
+
+entry!('goodman2016eu',
+  author('Bryce Goodman and Seth Flaxman'),
+  title('European Union regulations on algorithmic decision-making and a ``right to explanation\'\''),
+  arxiv(2016, '1606.08813'),
+  url('https://arxiv.org/abs/1606.08813'),
+nil)
+
+entry!('ribeiro2016lime',
+  kdd(2016),
+  title('``{W}hy Should {I} Trust You?\'\': Explaining the Predictions of Any Classifier'),
+  author('Marco Tulio Ribeiro and Sameer Singh and Carlos Guestrin'),
+  pages(1135, 1144),
+  organization('ACM'),
+nil)
+
+entry!('amershi2015modeltracker',
+  chi(2015),
+  title('Modeltracker: Redesigning performance analysis tools for machine learning'),
+  author('Saleema Amershi and Max Chickering and Steven M Drucker and Bongshin Lee and Patrice Simard and Jina Suh'),
+  pages(337, 346),
+  organization('ACM'),
+nil)
+
+entry!('shrikumar2016deeplift',
+  arxiv(2016, '1605.01713'),
+  title('Not just a black box: Learning important features through propagating activation differences'),
+  author('Avanti Shrikumar and Peyton Greenside and Anna Shcherbina and Anshul Kundaje'),
+nil)
+
+entry!('cook1980influence',
+  article('Technometrics', 1980, 22),
+  title('Characterizations of an empirical influence function for detecting influential cases in regression'),
+  author('R Dennis Cook and Sanford Weisberg'),
+  number(4),
+  pages(495, 508),
+  publisher('Taylor \& Francis Group'),
+nil)
