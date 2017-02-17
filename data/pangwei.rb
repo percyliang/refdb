7d4fa2babb1fed6d5941fd561083d07fdd53f422
Pang Wei Koh <pangwei@cs.stanford.edu>
2017-02-17 00:40:55 -0800

    Influence paper draft

--- data/pangwei.rb
+++ data/pangwei.rb
@@ -42,11 +42,189 @@ entry!('shrikumar2016deeplift',
   author('Avanti Shrikumar and Peyton Greenside and Anna Shcherbina and Anshul Kundaje'),
 nil)
 
+
+# entry!('lecun1998mnist',
+#   title('The {MNIST} database of handwritten digits'),
+#   author('Yann LeCun and Corinna Cortes and Christopher JC Burges'),
+#   year(1998),
+# nil)
+
+entry!('liu1989limited',
+  article('Mathematical programming', 1989, 45),
+  title('On the limited memory {BFGS} method for large scale optimization'),
+  author('Dong C Liu and Jorge Nocedal'),
+  number(1),
+  pages(503, 528),
+  publisher('Springer'),
+nil)
+
+entry!('lecun1998gradient',
+  article('Proceedings of the IEEE', 1998, 86),
+  title('Gradient-based learning applied to document recognition'),
+  author('Yann LeCun and L{\\\'e}on Bottou and Yoshua Bengio and Patrick Haffner'),
+  number(11),
+  pages(2278, 2324),
+  publisher('IEEE'),
+nil)
+
+entry!('springenberg2014striving',
+  arxiv(2014, '1412.6806'),
+  title('Striving for simplicity: The all convolutional net'),
+  author('Jost Tobias Springenberg and Alexey Dosovitskiy and Thomas Brox and Martin Riedmiller'),
+nil)
+
+entry!('kingma2014adam',
+  arxiv(2014, '1412.6980'),
+  title('Adam: A method for stochastic optimization'),
+  author('Diederik Kingma and Jimmy Ba'),
+nil)
+
+entry!('pearlmutter1994fast',
+  article('Neural computation', 1994, 6),
+  title('Fast exact multiplication by the Hessian'),
+  author('Barak A Pearlmutter'),
+  number(1),
+  pages(147, 160),
+  publisher('MIT Press'),
+nil)
+
+entry!('theano2016theano',
+  author('{Theano Development Team}'),
+  title('{Theano: A {Python} framework for fast computation of mathematical expressions}'),
+  arxiv(2016, '1605.02688'),
+  url('http://arxiv.org/abs/1605.02688'),
+nil)
+
+entry!('agarwal2016second',
+  arxiv(2016, '1602.03943'),
+  title('Second order stochastic optimization in linear time'),
+  author('Naman Agarwal and Brian Bullins and Elad Hazan'),
+nil)
+
+entry!('biggio2012poisoning',
+  icml(2012),
+  author('Battista Biggio and Blaine Nelson and Pavel Laskov'),
+  title('Poisoning Attacks Against Support Vector Machines'),
+  pages(1467, 1474),
+  url('http://dl.acm.org/citation.cfm?id=3042573.3042761'),  
+nil)
+
+entry!('li2016collab',
+  nips(2016),
+  title('Data Poisoning Attacks on Factorization-Based Collaborative Filtering'),
+  author('Bo Li and Yining Wang and Aarti Singh and Yevgeniy Vorobeychik'),
+  pages(1885, 1893),
+nil)
+
+entry!('cadamuro2016debugging',
+  author('Gabriel Cadamuro and Ran Gilad-Bachrach and Xiaojin Zhu'),
+  title('Debugging machine learning models'),
+  inproceedings('ICML Workshop on Reliable Machine Learning in the Wild', 2016),
+nil)
+
+entry!('frenay2014classification',
+  article('IEEE transactions on neural networks and learning systems', 2014, 25),
+  title('Classification in the presence of label noise: a survey'),
+  author('Beno{\^\i}t Fr{\\\'e}nay and Michel Verleysen'),
+  pages(845, 869),
+  publisher('IEEE'),
+nil)
+
+entry!('biggio2011label',
+  article('ACML', 2011, 20),
+  title('Support Vector Machines Under Adversarial Label Noise'),
+  author('Battista Biggio and Blaine Nelson and Pavel Laskov'),
+  pages(97, 112),
+nil)
+
+entry!('metsis2006enron',
+  inproceedings('CEAS', 2006),
+  title('Spam filtering with naive bayes-which naive bayes?'),
+  author('Vangelis Metsis and Ion Androutsopoulos and Georgios Paliouras'),
+  volume(17),
+  pages(28, 69),
+nil)
+
+entry!('jaeckel1972infinitesimal',
+  title('The infinitesimal jackknife'),
+  author('Louis A Jaeckel'),
+  article('Unpublished memorandum, Bell Telephone Laboratories, Murray Hill, NJ', 1972),
+  # year(1972),
+  # manual(1972),
+nil)
+
+# Cook's distance
+entry!('cook1977detection',
+  article('Technometrics', 1977, 19),
+  title('Detection of influential observation in linear regression'),
+  author('R Dennis Cook'),
+  pages(15, 18),
+nil)
+
 entry!('cook1980influence',
   article('Technometrics', 1980, 22),
   title('Characterizations of an empirical influence function for detecting influential cases in regression'),
   author('R Dennis Cook and Sanford Weisberg'),
-  number(4),
   pages(495, 508),
-  publisher('Taylor \& Francis Group'),
+nil)
+
+entry!('cook1982residuals',
+  title('Residuals and influence in regression'),
+  author('R Dennis Cook and Sanford Weisberg'),
+  book('New York: Chapman and Hall', 1982),
+nil)
+
+# This is more general stuff
+entry!('cook1986assessment',
+  article('Journal of the Royal Statistical Society. Series B (Methodological)', 1986),
+  title('Assessment of local influence'),
+  author('R Dennis Cook'),
+  pages(133, 169),
+  publisher('JSTOR'),
+nil)
+
+# entry!('mallows1975some',
+#   article('Unpublished memorandum, Bell Telephone Laboratories, Murray Hill, NJ', 1975, VOLUME),
+#   title('On some topics in robustness'),
+#   author('Colin L Mallows'),
+# nil)
+
+entry!('thomas1990assessing',
+  article('Technometrics', 1990, 32),
+  title('Assessing influence on predictions from generalized linear models'),
+  author('William Thomas and R Dennis Cook'),
+  number(1),
+  pages(59, 65),
+  publisher('Taylor \& Francis'),
+nil)
+
+entry!('wei1998generalized',
+  article('Scandinavian Journal of statistics', 1998, 25),
+  title('Generalized leverage and its applications'),
+  author('Bo-Cheng Wei and Yue-Qing Hu and Wing-Kam Fung'),
+  pages(25, 37),
+  publisher('Wiley Online Library'),
+nil)
+
+entry!('chatterjee1986influential',
+  article('Statistical Science', 1986),
+  title('Influential observations, high leverage points, and outliers in linear regression'),
+  author('Samprit Chatterjee and Ali S Hadi'),
+  pages(379, 393),
+  publisher('JSTOR'),
+nil)
+
+entry!('liu2014efficient',
+  icml(2014),
+  title('Efficient Approximation of Cross-Validation for Kernel Methods using Bouligand Influence Function'),
+  author('Yong Liu and Shali Jiang and Shizhong Liao'),
+  pages(324, 332),
+nil)
+
+entry!('debruyne2008model',
+  jmlr(2008, 9),
+  title('Model selection in kernel based regression using the influence function'),
+  author('Michiel Debruyne and Mia Hubert and Johan AK Suykens'),
+  number('Oct'),
+  pages(2377, 2400),
 nil)
