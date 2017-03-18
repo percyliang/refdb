bd698eb914afaf9d8d33f151aef96e0afd24bf8b
Arun Tejasvi Chaganty <chaganty@stanford.edu>
2017-03-18 15:58:47 -0700

    Added chaganty.rb

---
+++ data/chaganty.rb
@@ -0,0 +1,30 @@
+entry!('ratinov2011local',
+  author('Lee Ratinov and Dan Roth and D. Downey and M. Anderson'),
+  title('Local and Global Algorithms for Disambiguation to {W}ikipedia'),
+  acl(2011),
+  url('http://cogcomp.cs.illinois.edu/papers/RRDA11.pdf'),
+nil)
+entry!('kalyanpur2012structured',
+  author('Aditya Kalyanpur and Branimir K. Boguraev and Siddharth Patwardhan and J. William Murdock and Adam Lally and Christopher A. Welty and John M. Prager and Bonaventura Coppola and Achille Fokoue-Nkoutche and Lei Zhang and Yue Pan and Zhao Ming Qui'),
+  article('IBM Journal of Research and Development', 2012, 56), pages(351,364),
+  title('Structured data and inference in DeepQA'),
+  url('http://ieeexplore.ieee.org/xpls/abs{\_}all.jsp?arnumber=6177725'),
+nil)
+entry!('lee2015conversational',
+  title('Conversational knowledge teaching agent that uses a knowledge base'),
+  author('Kyusong Lee and Paul Hongsuck Seo and Junhwi Choi and Sangjun Koo and Gary Geunbae Lee'),
+  article('16th Annual Meeting of the Special Interest Group on Discourse and Dialogue', 2015), pages(139, 143),
+nil)
+
+entry!('han2015exploiting',
+  title('Exploiting knowledge base to generate responses for natural language dialog listening agents'),
+  author('Sangdo Han and Jeesoo Bang and Seonghan Ryu and Gary Geunbae Lee'),
+  article('16th Annual Meeting of the Special Interest Group on Discourse and Dialogue', 2015), pages(129, 133),
+nil)
+
+entry!('ellis2015tackbp',
+  title('TAC KBP 2015 Slot Descriptions'),
+  author('Joe Ellis and  Jeremy Getman and Heather Simpson and Kira Griffitt and Hoa Trang Dang and Ralph Grishman and Heng Ji and Catherine DePrince and Thomas Riese and  Neil Kuster'),
+  article('Linguistic Data Consortium', 2015),
+  url('https://tac.nist.gov//2015/KBP/ColdStart/guidelines/TAC_KBP_2015_Slot_Descriptions_V1.0.pdf'),
+nil)
