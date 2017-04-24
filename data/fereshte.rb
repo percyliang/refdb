bbf986af5f387d5f8cb6fe330b6051ac33fe0bb4
fereshte <fereshtekhani@gmail.com>
2017-04-24 14:13:14 -0700

    RSA papers

---
+++ data/fereshte.rb
@@ -0,0 +1,18 @@
+
+entry!('hawkins2015you',
+  inproceedings('CogSci', 2015),
+  title('Why do you ask? Good questions provoke informative answers.'),
+  author('Robert XD Hawkins and Andreas Stuhlm{\"u}ller and Judith Degen and Noah D Goodman'),
+nil)
+
+entry!('monroe2017colors',
+  arxiv(2017, '1703.10186'),
+  title('Colors in Context: A Pragmatic Neural Model for Grounded Language Understanding'),
+  author('Will Monroe and Robert XD Hawkins and Noah D Goodman and Christopher Potts'),
+nil)
+
+entry!('ullman2016pragmatics',
+  inproceedings('Proceedings of the 38th Annual Conference of the Cognitive Science Society', 2016),
+  title('The Pragmatics of Spatial Language'),
+  author('Tomer D Ullman and Yang Xu and Noah D Goodman'),
+nil)
