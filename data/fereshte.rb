961a83ebf77e722904d26de74fe72fff026bccd4
fereshte <fereshtekhani@gmail.com>
2017-05-04 16:16:42 -0700

    planning

--- data/fereshte.rb
+++ data/fereshte.rb
@@ -24,3 +24,28 @@ entry!('horn1984toward',
   pages(11, 42),
   publisher('Washington, DC: Georgetown University Press'),
 nil)
+
+
+
+entry!('allen2014reasoning',
+  title('Reasoning about plans'),
+  author('James Allen and Henry Kautz and Richard Pelavin and Josh Tenenberg'),
+  year(2014),
+  publisher('Morgan Kaufmann'),
+nil)
+
+entry!('allen2007plow',
+  aaai(2007),
+  title('Plow: A collaborative task learning agent'),
+  author('James Allen and Nathanael Chambers and George Ferguson and Lucian Galescu and Hyuckchul Jung and Mary Swift and William Taysom'),
+  pages(1514, 1519),
+nil)
+
+entry!('perrault1980plan',
+  computationalLinguistics(1980, 6),
+  title('A plan-based analysis of indirect speech acts'),
+  author('C Raymond Perrault and James F Allen'),
+  number('3-4'),
+  pages(167, 182),
+  publisher('MIT Press'),
+nil)
