29fbdbc41fcfa097adf554338a26cf547807d7b2
Tatsu Hashimoto <tatsu23456@gmail.com>
2017-07-26 10:46:33 -0700

    add thashim citations

---
+++ data/thashim.rb
@@ -0,0 +1,19 @@
+
+entry!('tversky86',
+       author('Amos Tversky and J. Wesley Hutchinson'),
+       title('Nearest neighbor analysis of psychological spaces'),
+       article('Psychological review', 1986), pages(1, 3),
+nil)
+
+entry!('gershman15',
+       author('Samuel J. Gershman and Joshua B. Tenenbaum'),
+       title('Phrase similarity in humans and machines'),
+       inproceedings('COGSCI',2015),
+nil)
+
+entry!('wood94',
+       author('Andrew T. Wood'),
+       title('Simulation of the von Mises Fisher distribution'),
+       article('Communications in statistics-simulation and computation', 1994),
+       pages(157, 164),
+nil)
