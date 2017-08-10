129b7a19b77d6ed856e484e190606eb8275e9910
Tatsu Hashimoto <tatsu23456@gmail.com>
2017-08-10 13:28:25 -0700

    citation update

--- data/thashim.rb
+++ data/thashim.rb
@@ -17,3 +17,38 @@ entry!('wood94',
        article('Communications in statistics-simulation and computation', 1994),
        pages(157, 164),
 nil)
+
+entry!('mueller17',
+       author('Jonas Mueller and David Gifford and Tommi Jaakkola'),
+       title('Sequence to better sequence: continuous revision of combinatorial structures'),
+       icml(2017),
+       pages(2536,2544),
+       nil)
+
+entry!('athanasopoulou14',
+       author('Georgia Athanasopoulou and Elias Iosif and Alexandros Potamianos'),
+       title('Low-Dimensional Manifold Distributional Semantic Models'),
+       coling(2014),
+       pages(731,740),
+       nil)
+
+entry!('andrews12',
+       author('Nocholas Andrews and Jason Eisner and Mark Dredze'),
+       title('Name phylogeny: A generative model of string variation'),
+       emnlp(2012),
+       pages(344,355),
+       nil)
+
+entry!('alexandrescu09',
+       author('Andrei Alexandrescu and Katrin Kirchhoff'),
+       title('Graph-based learning for statistical machine translation'),
+       naacl(2009),
+       pages(119,127),
+       nil)
+
+entry!('agirre2014semeval',
+       author('Eneko Agirre and Carmen Banea and Claire Cardie and Daniel M Cer and Mona T Diab and Aitor Gonzalez-Agirre and Weiwei Guo and Rada Mihalcea and German Rigau and Janyce Wiebe'),
+       title('SemEval-2014 Task 10: Multilingual Semantic Textual Similarity'),
+       coling(2014),
+       pages(81,91),
+       nil)
