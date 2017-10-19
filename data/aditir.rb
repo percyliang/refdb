12d22211b06177a6a82239130a8dec5ee37391cf
Jacob Steinhardt <jacob.steinhardt@gmail.com>
2017-10-19 14:50:00 -0700

    up

--- data/aditir.rb
+++ data/aditir.rb
@@ -1,21 +1,7 @@
 
-entry!('papernot2016distillation',
-  inproceedings('Security and Privacy (SP), 2016 IEEE Symposium on', 2016),
-  title('Distillation as a defense to adversarial perturbations against deep neural networks'),
-  author('Nicolas Papernot and Patrick McDaniel and Xi Wu and Somesh Jha and Ananthram Swami'),
-  pages(582, 597),
-  organization('IEEE'),
-nil)
-
 entry!('carlini2016defensive',
   arxiv(2016, '1607.04311'),
   title('Defensive distillation is not robust to adversarial examples'),
   author('Nicholas Carlini and David Wagner'),
-nil)
-
-entry!('tramer2017ensemble',
-  arxiv(2017, '1705.07204'),
-  title('Ensemble Adversarial Training: Attacks and Defenses'),
-  author('Florian Tram{\`e}r and Alexey Kurakin and Nicolas Papernot and Dan Boneh and Patrick McDaniel'),
 nil)
 
