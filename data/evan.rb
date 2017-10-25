80a597bb42bf8569d0c0b52bddd94e83064b5d7a
ezliu <evanliu@stanford.edu>
2017-10-24 21:13:47 -0700

    add refs

---
+++ data/evan.rb
@@ -0,0 +1,37 @@
+entry!('schaul2015prioritized',
+  title('Prioritized experience replay'),
+  author('T. Schaul and J. Quan and I. Antonoglou and D. Silver'),
+  iclr(2015),
+nil)
+
+entry!('bellemare2013arcade',
+  title('The Arcade Learning Environment: An evaluation platform for general agents'),
+  author('M. G. Bellemare and Y. Naddaf and J. Veness and M. Bowling'),
+  jair(2013, 47),
+  pages(253, 279),
+nil)
+
+entry!('silver2016mastering',
+  title('Mastering the game of Go with deep neural networks and tree search'),
+  author('D. Silver and A. Huang and C. J. Maddison and A. Guez and L. Sifre and G. Van Den Driessche and J. Schrittwieser and I. Antonoglou and V. Panneershelvam and M. Lanctot and others'),
+  article('Nature', 2016, 529),
+  number(7587),
+  pages(484, 489),
+  publisher('Nature Publishing Group'),
+nil)
+
+entry!('silver2017mastering',
+  title('Mastering the game of Go without human knowledge'),
+  author('Silver, David and Schrittwieser, Julian and Simonyan, Karen and Antonoglou, Ioannis and Huang, Aja and Guez, Arthur and Hubert, Thomas and Baker, Lucas and Lai, Matthew and Bolton, Adrian and others'),
+  article('Nature', 2017, 550),
+  number(7676),
+  pages(354, 359),
+  year(2017),
+  publisher('Nature Publishing Group'),
+nil)
+
+entry!('vecerik2017leveraging',
+  title('Leveraging Demonstrations for Deep Reinforcement Learning on Robotics Problems with Sparse Rewards'),
+  author('M. Vecer{\'\i)ki and T. Hester and J. Scholz and F. Wang and O. Pietquin and B. Piot and N. Heess and T. Roth{\"o)rl and T. Lampe and M. Riedmiller'),
+  arxiv(2017, '1707.08817'),
+nil)
