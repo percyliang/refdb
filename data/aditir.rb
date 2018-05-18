8cda67af683f532ce093abb57817958ed6732e71
Percy Liang <percyliang@gmail.com>
2018-05-18 09:33:52 -0700

    add

--- data/aditir.rb
+++ data/aditir.rb
@@ -7,12 +7,25 @@ nil)
 
 entry!('kolter2017provable',
   arxiv(2017, '1711.00851'),
-  title('Provable defenses against adversarial examples via the convex outer adversarial polytope'),
+  title('Provable defenses against adversarial examples via the convex outer adversarial polytope (published at {ICML} 2018)'),
   author('J Zico Kolter and Eric Wong'),
+nil)
+
+entry!('kolter2018provable',
+  icml(2018),
+  title('Provable defenses against adversarial examples via the convex outer adversarial polytope'),
+  author('Eric Wong and J. Zico Kolter'),
 nil)
 
 entry!('dvijotham2018dual',
   arxiv(2018, '1803.06567'),
   title('A Dual Approach to Scalable Verification of Deep Networks'),
   author('Krishnamurthy Dvijotham and Robert Stanforth and Sven Gowal and Timothy Mann and Pushmeet Kohli'),
+nil)
+
+entry!('hein2017formal',
+  nips(2017),
+  title('Formal guarantees on the robustness of a classifier against adversarial manipulation'),
+  author('Matthias Hein and Maksym Andriushchenko'),
+  pages(2263, 2273),
 nil)
