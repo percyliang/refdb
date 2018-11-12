cef650966cb1d56aa9710168348ad187dcf83095
Hugh Zhang <hughzhang@DN0a226374.SUNet>
2018-11-11 20:14:48 -0800

    hughs starter rb

---
+++ data/hugh.rb
@@ -0,0 +1,33 @@
+entry!('banerjee2005meteor',
+       title('METEOR: An automatic metric for mt evaluation with improved correlation with human judgments'),
+       author('Banerjee, Satanjeev and Lavie, Alon'),
+       acl(2005),
+ nil)
+entry!('languagegans2018',
+       title('Language GANs Falling Short'),
+       author('Massimo Caccia and Lucas Caccia and William Fedus and Hugo Larochelle and Joelle Pineau and Laurent Charlin '),
+       arxiv(2018),
+nil)
+entry!('fan2018hierarchical',
+       title('Hierarchical Neural Story Generation'),
+       author('Fan, Angela and Lewis, Mike and Dauphin, Yann'),
+       arxiv(2018),
+nil)
+entry!('theis2015note',
+       title('A note on the evaluation of generative models'),
+       author('Theis, Lucas and Oord, Aaron van den and Bethge, Matthias'),
+       arxiv(2015),
+nil)
+entry!('karras2017progressive',
+       title('Progressive growing of gans for improved quality, stability, and variation'),
+       author('Karras, Tero and Aila, Timo and Laine, Samuli and Lehtinen, Jaakko'),
+       arxiv(2017),
+nil)
+
+# in other places
+# wavenet
+# vae
+# edit model
+# gan
+# bleu
+
