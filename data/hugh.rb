72473ef18ee82c5f64d03047b07e86482f71b62a
Hugh Zhang <hughzhang@DN0a22a46a.SUNet>
2018-12-05 13:58:33 -0800

    more commits

--- data/hugh.rb
+++ data/hugh.rb
@@ -1,6 +1,6 @@
 entry!('banerjee2005meteor',
        title('METEOR: An automatic metric for mt evaluation with improved correlation with human judgments'),
-       author('Banerjee, Satanjeev and Lavie, Alon'),
+       author('Satanjeev Banerjee and Alon Lavie'),
        acl(2005),
  nil)
 entry!('languagegans2018',
@@ -10,24 +10,38 @@ entry!('languagegans2018',
 nil)
 entry!('fan2018hierarchical',
        title('Hierarchical Neural Story Generation'),
-       author('Fan, Angela and Lewis, Mike and Dauphin, Yann'),
+       author('Angela Fan and Mike Lewis and Yann Dauphin'),
        arxiv(2018),
 nil)
 entry!('theis2015note',
        title('A note on the evaluation of generative models'),
-       author('Theis, Lucas and Oord, Aaron van den and Bethge, Matthias'),
+       author('Lucas Theis and Aaron van den Oord and Matthias Bethge'),
        arxiv(2015),
 nil)
 entry!('karras2017progressive',
        title('Progressive growing of gans for improved quality, stability, and variation'),
-       author('Karras, Tero and Aila, Timo and Laine, Samuli and Lehtinen, Jaakko'),
+       author('Tero Karras and Timo Aila and Samuli Laine and Jaakko Lehtinen'),
        arxiv(2017),
 nil)
+entry!('sajjadi2018precision',
+       title('Assessing Generative Models via Precision and Recall'),
+       author('Mehdi S. M. Sajjadi, Olivier Bachem, Mario Lucic, Olivier Bousquet, Sylvain Gelly'),
+       arxiv(2018),
+nil)
+entry!('clarke2008diversity',
+  author('Charles L. A. Clarke, Maheedhar Kolla, Gordon V. Cormack, Olga Vechtomova, Azin Ashkan, Stefan Büttcher, Ian MacKinnon'),
+  inproceedings('ACM SIGIR', 2008),
+  title('Novelty and diversity in information retrieval evaluation'),
+  nil)
+entry!('nenkova2007pyramid',
+  author('Ani Nenkova, Rebecca J. Passonneau, Kathleen McKeown'),
+  inproceedings('ACM Transactions on Speech and Language Processing', 2007),
+  title('The Pyramid Method: Incorporating human content selection variation in summarization evaluation'),
+  nil)
+entry!('olsson2018skill',
+       title('Skill Rating for Generative Models'),
+       author('Catherine Olsson, Surya Bhupatiraju, Tom Brown, Augustus Odena, Ian Goodfellow'),
+       arxiv(2018),
+nil)
 
-# in other places
-# wavenet
-# vae
-# edit model
-# gan
-# bleu
 
