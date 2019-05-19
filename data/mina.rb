b26a0d77a0e2e7ca11612837938e107aa02772ce
Mina Lee <minalee@cs.stanford.edu>
2019-05-19 09:25:34 -0700

    synth bib

---
+++ data/mina.rb
@@ -0,0 +1,47 @@
+entry!('vadas2005programming',
+       author('David Vadas and James R. Curran'),
+       title('Programming With Unrestricted Natural Language'),
+       inproceedings(Name.new('Australasian Language Technology Workshop', 'ALTA'), 2005),
+       nil)
+
+entry!('armando2008sketch',
+       author('Armando Solar-Lezama'),
+       title('Program Synthesis by Sketching'),
+       phdthesis('University of California at Berkeley', 2008),
+       nil)
+
+entry!('parisotto2017sql',
+       author('Emilio Parisotto and Abdel-rahman Mohamed and Rishabh Singh and Lihong Li and Dengyong Zhou and Pushmeet Kohli'),
+       title('Neuro-symbolic Program Synthesis'),
+       iclr(2017),
+       nil)
+
+entry!('wang2017sql',
+       author('Chenglong Wang and Alvin Cheung and Rastislav Bodik'),
+       title('Synthesizing Highly Expressive {SQL} Queries from Input-Output Examples'),
+       pldi(2017),
+       nil)
+
+entry!('devlin2017robustfill',
+       author('Jacob Devlin and Jonathan Uesato and Surya Bhupatiraju and Rishabh Singh and Abdel-rahman Mohamed and Pushmeet Kohli'),
+       title('RobustFill: Neural Program Learning Under Noisy I/O'),
+       icml(2017),
+       nil)
+
+entry!('lin2018nl2bash',
+      author('Xi Victoria Lin and Chenglong Wang and Luke S. Zettlemoyer and Michael D. Ernst'),
+      title('NL2Bash: A Corpus and Semantic Parser for Natural Language Interface to the Linux Operating System'),
+      lrec(2018),
+      nil)
+
+entry!('iyer2018mapping',
+       author('Srinivasan Iyer and Ioannis Konstas and Alvin Cheung and Luke S. Zettlemoyer'),
+       title('Mapping Language to Code in Programmatic Context'),
+       emnlp(2018),
+       nil)
+
+entry!('bavishi2018api',
+       author('Rohan Bavishi and Caroline Lemieux and Neel Kant and Roy Fox and Koushik Sen and Ion Stoica'),
+       title('Neural Inference of {API} Functions from Input–Output Examples'),
+       nips(2018),
+       nil)
