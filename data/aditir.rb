0a35a74f7efcfe07f3605ed4d6c8106f273160d6
Aditi Raghunathan <aditir@stanford.edu>
2019-05-22 19:50:15 -0700

    added refs

--- data/aditir.rb
+++ data/aditir.rb
@@ -66,15 +66,145 @@ entry!('tsipras2018there',
   author('Dimitris Tsipras and Shibani Santurkar and Logan Engstrom and Alexander Turner and Aleksander Madry'),
 nil)
 
-entry!('zhang2019theoretically',
-  arxiv(2019, '1901.08573'),
-  title('Theoretically Principled Trade-off between Robustness and Accuracy'),
-  author('Hongyang Zhang and Yaodong Yu and Jiantao Jiao and Eric P Xing and Laurent El Ghaoui and Michael I Jordan'),
-nil)
-
 entry!('schmidt2018adversarially',
   nips(2018),
   title('Adversarially robust generalization requires more data'),
   author('Ludwig Schmidt and Shibani Santurkar and Dimitris Tsipras and Kunal Talwar and Aleksander Madry'),
   pages(5014, 5026),
+nil)
+
+entry!('zhang2019theoretically',
+  icml(2019), 
+  title('Theoretically principled trade-off between robustness and accuracy'),
+  author('Hongyang Zhang and Yaodong Yu and Jiantao Jiao and Eric P Xing and Laurent El Ghaoui and Michael I Jordan'),
+nil)
+
+entry!('zheng2016improving',
+  inproceedings('Proceedings of the ieee conference on computer vision and pattern recognition', 2016),
+  title('Improving the robustness of deep neural networks via stability training'),
+  author('Stephan Zheng and Yang Song and Thomas Leung and Ian Goodfellow'),
+  pages(4480, 4488),
+nil)
+
+entry!('cohen2019certified',
+       icml(2019),
+  title('Certified adversarial robustness via randomized smoothing'),
+  author('Jeremy M Cohen and Elan Rosenfeld and J Zico Kolter'),
+nil)
+
+entry!('rosenberg2005semi',
+ inproceedings('Proceedings of the Seventh IEEE Workshops on Application of
+ Computer Vision', 2005),
+ title('Semi-supervised self-training of object detection models'),
+ author('Chuck Rosenberg and Martial Hebert and Henry Schneiderman'),
+ nil)
+
+entry!('miyato2018virtual',
+  article('IEEE transactions on pattern analysis and machine intelligence', 2018, ),
+  title('Virtual adversarial training: a regularization method for supervised and semi-supervised learning'),
+  author('Takeru Miyato and Shin-ichi Maeda and Shin Ishii and Masanori Koyama'),
+  publisher('IEEE'),
+nil)
+
+entry!('cubuk2019autoaugment',
+  cvpr(2019), 
+  title('Autoaugment: Learning augmentation policies from data'),
+  author('Ekin D Cubuk and Barret Zoph and Dandelion Mane and Vijay Vasudevan and Quoc V Le'),
+nil)
+
+entry!('devries2017improved',
+  arxiv(2017, '1708.04552'),
+  title('Improved regularization of convolutional neural networks with cutout'),
+  author('Terrance DeVries and Graham W Taylor'),
+nil)
+
+entry!('torralba2008million',
+  article('IEEE transactions on pattern analysis and machine intelligence', 2008, 30),
+  title('80 million tiny images: A large data set for nonparametric object and scene recognition'),
+  author('Antonio Torralba and Rob Fergus and William T Freeman'),
+  number(11),
+  pages(1958, 1970),
+  publisher('IEEE'),
+nil)
+
+entry!('lecuyer2019certified',
+       inproceedings('In IEEE Symposium on Security and Privacy (SP)', 2019), 
+       title('Certified robustness to adversarial examples with differential privacy'),
+       author('Mathias Lecuyer and Vaggelis Atlidakis and Roxana Geambasu and Daniel Hsu and Suman Jana'),
+       nil)
+
+entry!('li2018second',
+  arxiv(2018, '1809.03113'),
+  title('Second-Order Adversarial Attack and Certifiable Robustness'),
+  author('Bai Li and Changyou Chen and Wenlin Wang and Lawrence Carin'),
+nil)
+
+entry!('zagoruyko2016wide',
+  inproceedings('British Machine Vision Conference', 2016),
+  title('Wide residual networks'),
+  author('Sergey Zagoruyko and Nikos Komodakis'),
+nil)
+
+entry!('oliver2018realistic',
+  nips(2018),
+  title('Realistic evaluation of deep semi-supervised learning algorithms'),
+  author('Avital Oliver and Augustus Odena and Colin A Raffel and Ekin Dogus Cubuk and Ian Goodfellow'),
+  pages(3235, 3246),
+nil)
+
+entry!('xie2019unsupervised',
+  arxiv(2019, '1904.12848'),
+  title('Unsupervised data augmentation'),
+  author('Qizhe Xie and Zihang Dai and Eduard Hovy and Minh-Thang Luong and Quoc V Le'),
+nil)
+
+entry!('laine2017temporal',
+  iclr(2017), 
+  title('Temporal ensembling for semi-supervised learning'),
+  author('Samuli Laine and Timo Aila'),
+nil)
+
+entry!('sajjadi2016regularization',
+  nips(2016),
+  title('Regularization with stochastic transformations and perturbations for deep semi-supervised learning'),
+  author('Mehdi Sajjadi and Mehran Javanmardi and Tolga Tasdizen'),
+  pages(1163, 1171),
+nil)
+
+entry!('tarvainen2017mean',
+  inproceedings('Advances in neural information processing systems', 2017),
+  title('Mean teachers are better role models: Weight-averaged consistency targets improve semi-supervised deep learning results'),
+  author('Antti Tarvainen and Harri Valpola'),
+  pages(1195, 1204),
+nil)
+
+entry!('zhu2003semi',
+  icml(2003),
+  title('Semi-supervised learning using gaussian fields and harmonic functions'),
+  author('Xiaojin Zhu and Zoubin Ghahramani and John D Lafferty'),
+  pages(912, 919),
+nil)
+
+entry!('lee2013pseudo',
+  icml(2013),
+  title('Pseudo-label: The simple and efficient semi-supervised learning method for deep neural networks'),
+  author('Dong-Hyun Lee'),
+nil)
+
+entry!('montasser2019vc',
+  arxiv(2019, '1902.04217'),
+  title('{VC} Classes are Adversarially Robustly Learnable, but Only Improperly'),
+  author('Omar Montasser and Steve Hanneke and Nathan Srebro'),
+nil)
+
+entry!('bubeck2019adversarial',
+  icml(2019), 
+  title('Adversarial examples from computational constraints'),
+  author('Sebastien Bubeck and Eric Price and Ilya Razenshteyn'),
+nil)
+
+entry!('degwekar2019computational',
+  arxiv(2019, '1902.01086'),
+  title('Computational Limitations in Robust Classification and Win-Win Results'),
+  author('Akshay Degwekar and Vinod Vaikuntanathan'),
 nil)
