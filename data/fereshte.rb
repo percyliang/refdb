58a276d1983f83f794c86426e41a36fb58c359db
fereshte- <fereshtekhani@gmail.com>
2019-08-15 14:33:24 -0700

    fairness refs

--- data/fereshte.rb
+++ data/fereshte.rb
@@ -577,3 +577,64 @@ entry!('becker2010economics',
  # year(2010),
   book('University of Chicago press', 2010),
 nil)
+
+entry!('corbett2018measure',
+  arxiv(2018, '1808.00023'),
+  title('The measure and mismeasure of fairness: A critical review of fair machine learning'),
+  author('Sam Corbett-Davies and Sharad Goel'),
+nil)
+
+entry!('arrow1973theory',
+  article('Discrimination in labor markets', 1973, 3),
+  title('The theory of discrimination'),
+  author('Kenneth Arrow and others'),
+  number(10),
+  pages(3, 33),
+  publisher('Princeton'),
+nil)
+
+entry!('phelps1972statistical',
+  article('The american economic review', 1972, 62),
+  title('The statistical theory of racism and sexism'),
+  author('Edmund S Phelps'),
+  number(4),
+  pages(659, 661),
+  publisher('JSTOR'),
+nil)
+
+entry!('madras2019fairness',
+  inproceedings('Proceedings of the Conference on Fairness, Accountability, and Transparency', 2019),
+  title('Fairness through causal awareness: Learning causal latent-variable models for biased data'),
+  author('David Madras and Elliot Creager and Toniann Pitassi and Richard Zemel'),
+  pages(349, 358),
+  organization('ACM'),
+nil)
+
+entry!('ustun2019fairness',
+  icml(2019),
+  title('Fairness without harm: Decoupled classifiers with preference guarantees'),
+  author('Berk Ustun and Yang Liu and David Parkes'),
+  pages(6373, 6382),
+nil)
+
+entry!('lipton2018does',
+  nips(2018),
+  title('Does mitigating ML\'s impact disparity require treatment disparity?'),
+  author('Zachary Lipton and Julian McAuley and Alexandra Chouldechova'),
+  pages(8125, 8135),
+nil)
+
+entry!('pearl2000causality',
+  title('Causality: models, reasoning and inference'),
+  author('Judea Pearl'),
+  volume(29),
+  #year(2000),
+  book('Springer',2000),
+nil)
+
+entry!('dwork2018decoupled',
+  inproceedings('Conference on Fairness, Accountability and Transparency', 2018),
+  title('Decoupled classifiers for group-fair and efficient machine learning'),
+  author('Cynthia Dwork and Nicole Immorlica and Adam Tauman Kalai and Max Leiserson'),
+  pages(119, 133),
+nil)
