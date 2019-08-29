fb3aae439356a42c6a51c81c9831e982a0c0cd5a
Shiori Sagawa <sagawas@stanford.edu>
2019-08-29 10:36:53 -0700

    add

---
+++ data/ssagawa.rb
@@ -0,0 +1,8 @@
+# coding: utf-8
+entry!('rockafellar2000optimization',
+       author('R. Tyrrell Rockafellar and Stanislav Uryasev'),
+       title('Optimization of Conditional Value-at-Risk'),
+       article('Journal of Risk', 2000, 2), pages(21, 41),
+nil)
+
+
