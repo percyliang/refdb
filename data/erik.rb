16ffae2b19d01a87a7705be76b2b37f2f60f59ae
Robin Jia <robinjia@stanford.edu>
2019-09-24 15:37:13 -0700

    add erik data

---
+++ data/erik.rb
@@ -0,0 +1,44 @@
+entry!('cheng2019robust',
+  acl(2019),
+  title('Robust Neural Machine Translation with Doubly Adversarial Inputs'),
+  author('Yong Cheng and Lu Jiang and Wolfgang Macherey'),
+nil)
+
+entry!('sakaguchi2017robsut',
+  aaai(2017),
+  title('Robsut Wrod Reocginiton via Semi-Character Recurrent Neural Network'),
+  author('Keisuke Sakaguchi and Kevin Duh and Matt Post and Benjamin Van Durme'),
+nil)
+
+entry!('gong2019context',
+  www(2019),
+  title('Context-Sensitive Malicious Spelling Error Correction'),
+  author('Hongyu Gong and Yuchen Li and Suma Bhat and Pramod Viswanath'),
+  pages(2771, 2777),
+nil)
+
+entry!('edizel2019misspelling',
+  naacl(2019),
+  title('Misspelling Oblivious Word Embeddings'),
+  author('Bora Edizel and Alekxandra Piktus and Piotr Bojanowski and Rui Ferreira and Edouard Grave and Fabrizio Silvestri'),
+nil)
+
+entry!('chen2019towards',
+  iieeeurosp(2019),
+  title('Towards Understanding Limitations of Pixel Discretization Against Adversarial Attacks'),
+  author('Jiefeng Chen and Xi Wu and Vaibhav Rastogi and Yingyu Liang and Somesh Jha'),
+nil)
+
+entry!('dolan2005mrpc',
+	iwp(2005),
+	title('Automatically constructing a corpus of sentential paraphrases'),
+	author('William B Dolan and Chris Brockett'),
+nil)
+
+entry!('davies2008COCA',
+  year('2008-2019'),
+  type('misc'),
+  howpublished('\url{https://www.english-corpora.org/faq.asp}'),
+  title('The Corpus of Contemporary American English (COCA): 560 million words, 1990-present'),
+  author('Mark Davies'),
+nil)
