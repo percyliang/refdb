06032fab6396990634f1d35e4fbb4a83662ff733
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2020-10-11 01:33:36 -0700

    Update w/ new Intex-Sempar 2020 references - Camera Ready

--- data/venues.rb
+++ data/venues.rb
@@ -137,6 +137,7 @@ def emnlpconll2012; [emnlpconll(2012), address('Jeju Island, South Korea')] end
 def conll(year); [inproceedings(Name.new('Computational Natural Language Learning', 'CoNLL'), year)] end
 def conll2010; [conll(2010), address('Uppsala, Sweden')] end
 def lrec(year); [inproceedings(Name.new('Language Resources and Evaluation Conference', 'LREC'), year)] end
+def emnlpfindings(year); [inproceedings(Name.new('Findings of Empirical Methods in Natural Language Processing', 'Findings of EMNLP'), year)] end
 
 def icassp(year); [inproceedings(Name.new('International Conference on Acoustics, Speech, and Signal Processing', 'ICASSP'), year)] end
 def icslp(year); [inproceedings(Name.new('International Conference on Spoken Language Processing', 'ICSLP'), year)] end
