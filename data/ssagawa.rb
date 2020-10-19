f95ceb00ecae0837645caac1237ea0fbe94167de
Shiori Sagawa <sagawas@stanford.edu>
2020-10-18 23:18:11 -0700

    add

--- data/ssagawa.rb
+++ data/ssagawa.rb
@@ -22,3 +22,51 @@ entry!('azzalini2012some',
   pages(857, 879),
   publisher('Springer'),
 nil)
+
+entry!('ni2019justifying',
+  emnlp(2019),
+  title('Justifying recommendations using distantly-labeled reviews and fine-grained aspects'),
+  author('Jianmo Ni and Jiacheng Li and Julian McAuley'),
+  pages(188, 197),
+nil)
+
+entry!('zhang2020adaptive',
+  arxiv(2020, '2007.02931'),
+  title('Adaptive Risk Minimization: A Meta-Learning Approach for Tackling Group Shift'),
+  author('Marvin Zhang and Henrik Marklund and Abhishek Gupta and Sergey Levine and Chelsea Finn'),
+nil)
+
+entry!('caldas2018leaf',
+  arxiv(2018, '1812.01097'),
+  title('Leaf: A benchmark for federated settings'),
+  author('Sebastian Caldas and Peter Wu and Tian Li and Jakub Kone{\v{c}}n{\`y} and H Brendan McMahan and Virginia Smith and Ameet Talwalkar'),
+nil)
+
+entry!('li2019fair',
+  arxiv(2019, '1905.10497'),
+  title('Fair resource allocation in federated learning'),
+  author('Tian Li and Maziar Sanjabi and Ahmad Beirami and Virginia Smith'),
+nil)
+
+entry!('west2014exploiting',
+  tacl(2014, 2),
+  title('Exploiting social network structure for person-to-person sentiment analysis'),
+  author('Robert West and Hristo S Paskov and Jure Leskovec and Christopher Potts'),
+  pages(297, 310),
+  publisher('MIT Press'),
+nil)
+
+entry!('patro2020fairrec',
+  inproceedings('Proceedings of The Web Conference 2020', 2020),
+  title('FairRec: Two-Sided Fairness for Personalized Recommendations in Two-Sided Platforms'),
+  author('Gourab K Patro and Arpita Biswas and Niloy Ganguly and Krishna P Gummadi and Abhijnan Chakraborty'),
+  pages(1194, 1204),
+nil)
+
+entry!('lau2014social',
+  article('Decision Support Systems', 2014, 65),
+  title('Social analytics: Learning fuzzy product ontologies for aspect-oriented sentiment analysis'),
+  author('Raymond YK Lau and Chunping Li and Stephen SY Liao'),
+  pages(80, 94),
+  publisher('Elsevier'),
+nil)
