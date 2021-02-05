198824f4a9460eb4d61b3ea3666d2caecd9fead6
Michihiro Yasunaga <gomagoma353@gmail.com>
2021-02-04 21:51:16 -0800

    add

---
+++ data/michi.rb
@@ -0,0 +1,137 @@
+entry!('seo2014program',
+  icse(2014),
+  title('Programmers\' Build Errors: A Case Study at Google'),
+  author('Hyunmin Seo and Caitlin Sadowski and Sebastian Elbaum and Edward Aftandilian and Robert Bowdidge'),
+)
+
+entry!('ding2020patching',
+  ase(2020),
+  title('Patching as Translation: the Data and the Metaphor'),
+  author('Yangruibo Ding and Baishakhi Ray and Premkumar Devanbu and Vincent J. Hellendoorn'),
+)
+
+entry!('hajipour2019samplefix',
+  arxiv(2019, '1906.10502'),
+  title('SampleFix: Learning to Correct Programs by Sampling Diverse Fixes'),
+  author('Hajipour, Hossein and Bhattacharya, Apratim and Fritz, Mario'),
+)
+
+entry!('gupta2019deep',
+  aaai(2019),
+  title('Deep reinforcement learning for programming language correction'),
+  author('Rahul Gupta and Aditya Kanade and Shirish Shevade'),
+)
+
+entry!('pradel2018deepbugs',
+  oopsla(2018),
+  title('Deepbugs: A learning approach to name-based bug detection'),
+  author('Pradel, Michael and Sen, Koushik'),
+)
+
+entry!('ahmed2018compilation',
+  icse(2018),
+  title('Compilation error repair: for the student programs, from the student programs'),
+  author('Ahmed, Umair Z and Kumar, Pawan and Karkare, Amey and Kar, Purushottam and Gulwani, Sumit'),
+)
+
+entry!('bader2019getafix',
+  oopsla(2019),
+  title('Getafix: Learning to fix bugs automatically'),
+  author('Bader, Johannes and Scott, Andrew and Pradel, Michael and Chandra, Satish'),
+)
+
+
+entry!('edunov2018understanding',
+  emnlp(2018),
+  title('Understanding back-translation at scale'),
+  author('Edunov, Sergey and Ott, Myle and Auli, Michael and Grangier, David'),
+)
+
+entry!('artetxe2018unsupervised',
+  arxiv(2018, '1809.01272'),
+  title('Unsupervised statistical machine translation'),
+  author('Artetxe, Mikel and Labaka, Gorka and Agirre, Eneko'),
+)
+
+entry!('lachaux2020unsupervised',
+  neurips(2020),
+  title('Unsupervised Translation of Programming Languages'),
+  author('Lachaux, Marie-Anne and Roziere, Baptiste and Chanussot, Lowik and Lample, Guillaume'),
+)
+
+entry!('lample2018phrase',
+  emnlp(2018),
+  title('Phrase-based \& neural unsupervised machine translation'),
+  author('Lample, Guillaume and Ott, Myle and Conneau, Alexis and Denoyer, Ludovic and Ranzato, Marc\'Aurelio'),
+)
+
+entry!('yang2020improving',
+  icml(2020),
+  title('Improving Molecular Design by Stochastic Iterative Target Augmentation'),
+  author('Yang, Kevin and Jin, Wengong and Swanson, Kyle and Barzilay, Regina and Jaakkola, Tommi'),
+)
+entry!('jin2019learning',
+  iclr(2019),
+  title('Learning multimodal graph-to-graph translation for molecular optimization'),
+  author('Jin, Wengong and Yang, Kevin and Barzilay, Regina and Jaakkola, Tommi'),
+)
+
+entry!('min2019discrete',
+  emnlp(2019),
+  title('A discrete hard em approach for weakly supervised question answering'),
+  author('Min, Sewon and Chen, Danqi and Hajishirzi, Hannaneh and Zettlemoyer, Luke'),
+)
+
+entry!('zhong2020grounded',
+  emnlp(2020),
+  title('Grounded adaptation for zero-shot executable semantic parsing'),
+  author('Min, Sewon and Chen, Danqi and Hajishirzi, Hannaneh and Zettlemoyer, Luke'),
+)
+
+entry!('yasunaga2017graph',
+  conll(2017),
+  title('Graph-based neural multi-document summarization'),
+  author('Yasunaga, Michihiro and Zhang, Rui and Meelu, Kshitijh and Pareek, Ayush and Srinivasan, Krishnan and Radev, Dragomir'),
+)
+
+entry!('xie2020unsupervised',
+  neurips(2020),
+  title('Unsupervised data augmentation for consistency training'),
+  author('Xie, Qizhe and Dai, Zihang and Hovy, Eduard and Luong, Minh-Thang and Le, Quoc V'),
+)
+
+entry!('antoniou2017data',
+  arxiv(2017, '1711.04340'),
+  title('Data augmentation generative adversarial networks'),
+  author('Antoniou, Antreas and Storkey, Amos and Edwards, Harrison'),
+)
+
+entry!('mcclosky2006effective',
+  naacl(2006),
+  title('Effective self-training for parsing'),
+  author('McClosky, David and Charniak, Eugene and Johnson, Mark'),
+)
+
+entry!('xu2020autoqa',
+  emnlp(2020),
+  title('Auto{QA}: From databases to {QA} semantic parsers with only synthetic training data'),
+  author('Xu, Silei and Semnani, Sina J and Campagna, Giovanni and Lam, Monica S'),
+)
+
+entry!('hellendoorn2019code',
+  icse(2019),
+  title('When code completion fails: A case study on real-world completions'),
+  author('Hellendoorn, Vincent J and Proksch, Sebastian and Gall, Harald C and Bacchelli, Alberto'),
+)
+
+entry!('sun2016deep',
+  eccv(2016),
+  title('Deep coral: Correlation alignment for deep domain adaptation'),
+  author('Sun, Baochen and Saenko, Kate'),
+)
+
+entry!('pascanu2013difficulty',
+  icml(2013),
+  title('On the difficulty of training recurrent neural networks'),
+  author('Pascanu, Razvan and Mikolov, Tomas and Bengio, Yoshua'),
+)
