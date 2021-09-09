c90daa7525a0b4755d194c54ae6945055a009b64
i-gao <igao@stanford.edu>
2021-09-09 16:30:09 -0700

    add

---
+++ data/irena.rb
@@ -0,0 +1,5 @@
+entry!('berthelot2021adamatch',
+  arxiv(2021, '2106.04732'),
+  title('AdaMatch: A Unified Approach to Semi-Supervised Learning and Domain Adaptation'),
+  author('David Berthelot and Rebecca Roelofs and Kihyuk Sohn and Nicholas Carlini and Alex Kurakin'),
+)
