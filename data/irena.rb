cc67f2318e3c974f15b79027c5fc33979a73fff7
i-gao <igao@stanford.edu>
2021-09-23 13:33:31 -0700

    add

--- data/irena.rb
+++ data/irena.rb
@@ -3,3 +3,10 @@ entry!('berthelot2021adamatch',
   title('AdaMatch: A Unified Approach to Semi-Supervised Learning and Domain Adaptation'),
   author('David Berthelot and Rebecca Roelofs and Kihyuk Sohn and Nicholas Carlini and Alex Kurakin'),
 )
+
+entry!('cubuk2020randaugment',
+  cvpr(2020),
+  title('Randaugment: Practical automated data augmentation with a reduced search space'),
+  author('Ekin D Cubuk and Barret Zoph and Jonathon Shlens and Quoc V Le'),
+  pages(702, 703),
+)
