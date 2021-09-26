83333faf0b9d3982e8bf16a48f981acbfcf81fcf
Michael Xie <sangxie513@gmail.com>
2021-09-25 22:39:17 -0500

    add

--- data/sxie.rb
+++ data/sxie.rb
@@ -503,4 +503,27 @@ entry!('reed2021self',
   arxiv(2021)
       )
 
+entry!('wei2021finetuned',
+       author('Jason Wei and Maarten Bosma and Vincent Y. Zhao and Kelvin Guu and Adams Wei Yu and Brian Lester and Nan Du and Andrew M. Dai and Quoc V. Le'),
+       title('Finetuned Language Models Are Zero-Shot Learners'),
+  arxiv(2021)
+      )
+
+entry!('gao2021making',
+       author('Tianyu Gao and Adam Fisch and Danqi Chen'),
+       title('Making Pre-trained Language Models Better Few-shot Learners'),
+  arxiv(2021)
+      )
+
+entry!('kleijn2012bernstein',
+       author('B.J.K. Kleijn and A.W. van der Vaart'),
+       title('The {Bernstein}-von Mises theorem under misspecification'),
+  article('Electronic Journal of Statistics', 2012, 6),
+      )
+
+entry!('wei2021why',
+       author('Colin Wei and Sang Michael Xie and Tengyu Ma'),
+       title('Why Do Pretrained Language Models Help in Downstream Tasks? An Analysis of Head and Prompt Tuning'),
+  arxiv(2021)
+      )
 
