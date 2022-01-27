b5763040bf43c5dabebe64fcc40969a9f3a4f367
ejones313 <erik.jones313@gmail.com>
2022-01-26 23:27:32 -0800

    add

--- data/erik.rb
+++ data/erik.rb
@@ -1,3 +1,232 @@
+entry!('liu2021what',
+  arxiv(2021, '2101.06804'),
+  title('What Makes Good In-Context Examples for {GPT}-3'),
+  author('Jiachang Liu and Dinghan Shen and Yizhe Zhang and Bill Dolan and Lawrence Carin and Weizhu Chen'),
+  )
+
+entry!('meyer2014semantic',
+  article('Science', 2014, 345),
+  title('Semantic priming well established'),
+  author('David E. Meyer'),
+  number(6196),
+  pages(523, 523),
+  publisher('American Association for the Advancement of Science'),
+)
+
+entry!('weidinger2021ethical',
+  arxiv(2021, '2112.04359'),
+  title('Ethical and social risks of harm from Language Models'),
+  author('Laura Weidinger and John Mellor and Maribeth Rauh and Conor Griffin and Jonathan Uesato and Po-Sen Huang and Myra Cheng and Mia Glaese and Borja Balle and Atoosa Kasirzadeh and Zac Kenton and Sasha Brown and Will Hawkins and Tom Stepleton and Courtney Biles and Abeba Birhane and Julia Haas and Laura Rimell and Lisa Anne Hendricks and William Isaac and Sean Legassick and Geoffrey Irving and Iason Gabriel'),
+  )
+
+entry!('bender2021stochastic',
+  facct(2021),
+  title('On the Dangers of Stochastic Parrots: Can Language Models Be Too Big?'),
+  author('Emily Bender and Timnit Gebru and Angelina McMillan-Major and Shmargaret Shmitchel'),
+  )
+
+entry!('carlini2020extracting',
+  arxiv(2020, '2012.07805'),
+  title('Extracting Training Data from Large Language Models'),
+  author('Nicholas Carlini and Florian Tramer and Eric Wallace and Matthew Jagielski and Ariel Herbert-Voss and Katherine Lee and Adam Roberts and Tom Brown and Dawn Song and Ulfar Erlingsson and Alina Oprea and Colin Raffel'),
+  )
+
+entry!('li2021hidden',
+  arxiv(2021, '2105.00164'),
+  title('Hidden Backdoors in Human-Centric Language Models'),
+  author('Shaofeng Li and Hui Liu and Tian Dong and Benjamin Zi Hao Zhao and Minhui Xue and Haojin Zhu and Jialiang Lu'),
+  )
+
+entry!('jacobs2021measurement',
+  facct(2021),
+  title('Measurement and Fairness'),
+  author('Abigail Z. Jacobs and Hanna Wallach'),
+  )
+
+entry!('groenwold2020investigating',
+  emnlp(2020),
+  title('Investigating African-American Vernacular English in Transformer-Based Text Generation'),
+  author('Sophie Groenwold and Lily Ou and Aesha Parekh and Samhita Honnavalli and Sharon Levy and Diba Mirza and William Yang Wang'),
+  )
+
+entry!('blodgett2021stereotyping',
+  acl(2021),
+  title('Stereotyping Norwegian Salmon: An Inventory of Pitfalls in Fairness Benchmark Datasets'),
+  author('Su Lin Blodgett and Gilsinia Lopez and Alexandra Olteanu and Robert Sim and Hanna Wallach'),
+  )
+
+entry!('sheng2019woman',
+  emnlp(2019),
+  title('The Woman Worked as a Babysitter: On Biases in Language Generation'),
+  author('Emily Sheng and Kai-Wei Chang and Premkumar Natarajan and Nanyun Peng'),
+  )
+
+entry!('hendrycks2021coding',
+  neurips(2021),
+  title('Measuring Coding Challenge Competence With {APPS}'),
+  author('Dan Hendrycks and Steven Basart and Saurav Kadavath and Mantas Mazeika and Akul Arora and Ethan Guo and Collin Burns and Samir Puranik and Horace He and Dawn Song and Jacob Steinhardt'),
+  )
+
+entry!('krishna2021hurdles',
+  naacl(2021),
+  title('Hurdles to Progress in Long-form Question Answering'),
+  author('Kalpesh Krishna and Aurko Roy and Mohit Iyyer'),
+  )
+
+entry!('shuster2021retrieval',
+  arxiv(2021, '2104.07567'),
+  title('Retrieval Augmentation Reduces Hallucination in Conversation'),
+  author('Kurt Shuster and Spencer Poff and Moya Chen and Douwe Kiela and Jason Weston'),
+  )
+
+entry!('gabriel2021go',
+  aclfindings(2021),
+  title('{GO FIGURE}: A Meta Evaluation of Factuality in Summarization'),
+  author('Saadia Gabriel and Asli Celikyilmaz and Rahul Jha and Yejin Choi and Jianfeng Gao'),
+  )
+
+entry!('lin2021truthful',
+  arxiv(2021, '2109.07958'),
+  title('TruthfulQA: Measuring How Models Mimic Human Falsehoods'),
+  author('Stephanie Lin and Jacob Hilton and Owain Evans'),
+  )
+
+entry!('cobbe2021training',
+  arxiv(2021, '2110.14168'),
+  title('Training Verifiers to Solve Math Word Problems'),
+  author('Karl Cobbe and Vineet Kosaraju and Mohammad Bavarian and Mark Chen and Heewoo Jun and Lukasz Kaiser and Matthias Plappert and Jerry Tworek and Jacob Hilton and Reiichiro Nakano and Christopher Hesse and John Schulman'),
+  )
+
+entry!('hendrycks2021math',
+  neurips(2021),
+  title('Measuring Mathematical Problem Solving With the {MATH} Dataset'),
+  author("Dan Hendrycks and Collin Burns and Saurav Kadavath and Akul Arora and Steven Basart and Eric Tang and Dawn Song and Jacob Steinhardt"),
+  )
+
+entry!('tang2021solving',
+  arxiv(2021, '2111.08276'),
+  title('Solving Probability and Statistics Problems by Program Synthesis'),
+  author('Leonard Tang and Elizabeth Ke and Nikhil Singh and Nakul Verma and Iddo Drori'),
+  )
+
+entry!('fan2019eli5',
+  acl(2019),
+  title('{ELI5}: Long Form Question Answering'),
+  author('Angela Fan and Yacine Jernite and Ethan Perez and David Grangier and Jason Weston and Michael Auli'),
+  )
+
+entry!('zhou2020learning',
+  aaai(2020),
+  title('Learning to Compare for Better Training and Evaluation of Open Domain Natural Language Generation Models'),
+  author("Wangchunshu Zhou and Ke Xu"),
+  )
+
+entry!('ram2018conversational',
+  arxiv(2018, '1801.03604'),
+  title('Conversational AI: The Science Behind the Alexa Prize'),
+  author("Ashwin Ram and Rohit Prasad and Chandra Khatri and Anu Venkatesh and Raefer Gabriel and Qing Liu and Jeff Nunn and Behnam Hedayatnia and Ming Cheng and Ashish Nagar and Eric King and Kate Bland and Amanda Wartick and Yi Pan and Han Song and Sk Jayadevan and Gene Hwang and Art Pettigrue"),
+  )
+
+entry!('rothe2020leveraging',
+  tacl(2020,8),
+  title('Leveraging Pre-trained Checkpoints for Sequence Generation Tasks'),
+  author('Sascha Rothe and Shashi Narayan and Aliaksei Severyn'),
+  pages(264, 280),
+)
+
+entry!('ziegler2019fine',
+  arxiv(2019, '1909.08593'),
+  title('Fine-Tuning Language Models from Human Preferences'),
+  author('Daniel M. Ziegler and Nisan Stiennon and Jeffrey Wu and Tom B. Brown and Alec Radford and Dario Amodei and Paul Christiano and Geoffrey Irving'),
+  )
+
+entry!('steinnon2020learning',
+  title('Learning to summarize from human feedback'),
+  author('Nisan Stiennon and Long Ouyang and Jeff Wu and Daniel M. Ziegler and Ryan Lowe and Chelsea Voss and Alec Radford and Dario Amodei and Paul Christiano'),
+  neurips(2020),
+  )
+
+entry!('strack1988priming',
+  article('European Journal of Social Psychology', 1988, 18),
+  title('Priming and communication: Social determinants of information use in judgments of life satisfaction'),
+  author('Fritz Strack and Leonard L. Martin and Nobert Schwarz'),
+  number(5),
+  pages(429, 442),
+  publisher('John Wiley \& Sons'),
+  )
+
+entry!('kahneman2002representativeness',
+  title('Representativeness Revisited: Attribute Substitution in Intuitive Judgment'),
+  author('Daniel Kahneman and Shane Frederick'),
+  incollection('Heuristics and Biases: The Psychology of Intuitive Judgement', 2002),
+  editor('Thomas Gilovich and Dale Griffin and Daniel Kahneman'),
+  pages(49, 81),
+  publisher('Cambridge University Press'), address('Cambridge'),
+  )
+
+entry!('tversky1973availability',
+  article('Cognitive Psychology', 1973, 5),
+  title('Availability: A heuristic for judging frequency and probability'),
+  author('Amos Tversky and Daniel Kahneman'),
+  number(2),
+  pages(207, 232),
+  publisher('Elsevier'),
+)
+
+entry!('windhager2010laying',
+  article('Collegium Antropologicum', 2010, 34),
+  title('Laying eyes on Headlights: Eye Movements Suggest Facial Features in Cars'),
+  author('Sonja Windhager and Florian Hutzler and Claus-Christian Carbon and Elisabeth Oberzaucher and Katrin Schaefer and Truls Thorstensen and Helmut Leder and Karl Grammer'),
+  number(3),
+  pages(1075,1080),
+  publisher('Croatian Anthropological Society')
+)
+
+entry!('tversky1974judgement',
+  article('Science', 1974, 185),
+  title('Judgment under Uncertainty: Heuristics and Biases'),
+  author('Amos Tversky and Daniel Kahneman'),
+  number(4157),
+  pages(1124, 1131),
+  publisher('American Association for the Advancement of Science'),
+)
+
+entry!('jacowitz1995anchoring',
+  article('Personality and Social Psychology Bulletin', 1995, 21),
+  title('Measures of Anchoring in Estimation Tasks'),
+  author('Karen E. Jacowitz and Daniel Kahneman'),
+  number(11),
+  pages(1161, 1166),
+  publisher('SAGE Publications')
+  )
+
+entry!('hendrycks2021measuring',
+  iclr(2021),
+  title('Measuring Massive Multitask Language Understanding'),
+  author('Dan Hendrycks and Collin Burns and Steven Basart and Andy Zou and Mantas Mazeika and Dawn Song and Jacob Steinhardt'),
+  )
+
+entry!('tversky1981framing',
+  article('Science', 1981, 211),
+  title('The Framing of Decisions and the Psychology of Choice'),
+  author('Amos Tversky and Daniel Kahneman'),
+  number(4481),
+  pages(453, 458),
+  publisher('American Association for the Advancement of Science'),
+)
+
+entry!('rae2021gopher', 
+  arxiv(2021, '2112.11446'),
+  title('Scaling Language Models: Methods, Analysis \& Insignts from Training Gopher'),
+  author("Jack W. Rae and Sebastian Borgeaud and Trevor Cai and Katie Millican and Jordan Hoffmann and Francis Song and John Aslanides and Sarah Henderson and Roman Ring and Susannah Young and Eliza Rutherford and Tom Hennigan and Jacob Menick and Albin Cassirer and Richard Powell and George van den Driessche and Lisa Anne Hendricks and Maribeth Rauh and Po-Sen Huang and Amelia Glaese and Johannes Welbl and Sumanth Dathathri and Saffron Huang and Jonathan Uesato and John Mellor and Irina Higgins and Antonia Creswell and Nat McAleese and Amy Wu and Erich Elsen and Siddhant Jayakumar and Elena Buchatskaya and David Budden and Esme Sutherland and Karen Simonyan and Michela Paganini and Laurent Sifre and Lena Martens and Xiang Lorraine Li and Adhiguna Kuncoro and Aida Nematzadeh and Elena Gribovskaya and Domenic Donato and Angeliki Lazaridou and Arthur Mensch and Jean-Baptiste Lespiau and Maria Tsimpoukelli and Nikolai Grigorev and Doug Fritz and Thibault Sottiaux and Mantas Pajarskas and Toby Pohlen and Zhitao Gong and Daniel Toyama and Cyprien de Masson d'Autume and Yujia Li and Tayfun Terzi and Vladimir Mikulik and Igor Babuschkin and Aidan Clark and Diego de Las Casas and Aurelia Guy and Chris Jones and James Bradbury and Matthew Johnson and Blake Hechtman and Laura Weidinger and Iason Gabriel and William Isaac and Ed Lockhart and Simon Osindero and Laura Rimell and Chris Dyer and Oriol Vinyals and Kareem Ayoub and Jeff Stanway and Lorrayne Bennett and Demis Hassabis and Koray Kavukcuoglu and Geoffrey Irving")
+  )
+
+entry!('chen2021codex',
+  arxiv(2021, '2107.03374'),
+  title('Evaluating Large Language Models Trained on Code'),
+  author('Mark Chen and Jerry Tworek and Heewoo Jun and Qiming Yuan and Henrique Ponde de Oliveira Pinto and Jared Kaplan and Harri Edwards and Yuri Burda and Nicholas Joseph and Greg Brockman and Alex Ray and Raul Puri and Gretchen Krueger and Michael Petrov and Heidy Khlaaf and Girish Sastry and Pamela Mishkin and Brooke Chan and Scott Gray and Nick Ryder and Mikhail Pavlov and Alethea Power and Lukasz Kaiser and Mohammad Bavarian and Clemens Winter and Philippe Tillet and Felipe Petroski Such and Dave Cummings and Matthias Plappert and Fotios Chantzis and Elizabeth Barnes and Ariel Herbert-Voss and William Hebgen Guss and Alex Nichol and Alex Paino and Nikolas Tezak and Jie Tang and Igor Babuschkin and Suchir Balaji and Shantanu Jain and William Saunders and Christopher Hesse and Andrew N. Carr and Jan Leike and Josh Achiam and Vedant Misra and Evan Morikawa and Alec Radford and Matthew Knight and Miles Brundage and Mira Murati and Katie Mayer and Peter Welinder and Bob McGrew and Dario Amodei and Sam McCandlish and Ilya Sutskever and Wojciech Zaremba')
+  )
+
 entry!('zhou2016cams',
   cvpr(2016),
   title('Learning Deep Features for Discriminative Localization'),
